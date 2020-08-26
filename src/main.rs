708d621b64893b0a13b368fddc708367fc8f594f
marshal20 <x.madkour@gmail.com>
2020-08-26 21:57:06 +0200

    added multisampling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::path::Path;
 use std::fs::File;
 use std::io::BufWriter;
+use rand::Rng;
 
-type Real = f64;
+type Real = f32;
 
 #[derive(Copy, Clone)]
 struct Complex {
@@ -32,6 +33,22 @@ struct Color {
     a: Real,
 }
 
+impl Color {
+    fn add(&mut self, rhs: Color) {
+        self.r += rhs.r;
+        self.g += rhs.g;
+        self.b += rhs.b;
+        self.a += rhs.a;
+    }
+
+    fn divide(&mut self, value: Real) {
+        self.r /= value;
+        self.g /= value;
+        self.b /= value;
+        self.a /= value;
+    }
+}
+
 fn save_image(color_buffer: &[Color], width: usize, height: usize, path: &str) {
     let path = Path::new(path);
     let file = File::create(path).unwrap();
@@ -58,6 +75,7 @@ fn save_image(color_buffer: &[Color], width: usize, height: usize, path: &str) {
 fn main() {
     const BUFFER_WIDTH: usize = 1024;
     const BUFFER_HEIGHT: usize = 1024;
+    const SAMPLE_COUNT: usize = 16;
     const CENTER_X: Real = -0.7453;
     const CENTER_Y: Real = 0.1127;
     const VIEW_WIDTH: Real = 6.5E-4;
@@ -86,21 +104,28 @@ fn main() {
 
     let mut color_buffer = vec![Color {r:0.0, g:0.0, b:0.0, a:0.0}; BUFFER_WIDTH * BUFFER_HEIGHT]; // Row major
 
+    let mut rng = rand::thread_rng();
     println!("Drawing the buffer...");
+
     for y in 0..BUFFER_HEIGHT {
         for x in 0..BUFFER_WIDTH {
-            let norm_pos_x = (x as Real)/(BUFFER_WIDTH as Real) * 2.0 - 1.0; // [-1:1]
-            let norm_pos_y = (y as Real)/(BUFFER_HEIGHT as Real) * 2.0 - 1.0; // [-1:1]
-            let mut pos = Complex {r: 0.0, i: 0.0};
-            pos.r = CENTER_X + norm_pos_x * VIEW_WIDTH / 2.0; // real axis
-            pos.i = CENTER_Y + norm_pos_y * VIEW_HEIGHT / 2.0; // imaginary axis
-            let mut iterations: u32 = 0;
-            let mut temp = Complex {r: 0.0, i: 0.0};
-            while temp.length() <= MAX_LENGTH && iterations < MAX_ITERATIONS {
-                temp = temp.squared().add(&pos);
-                iterations += 1;
+            let mut pixel_color = Color {r:0.0, g:0.0, b:0.0, a:0.0};
+            for _ in 0..SAMPLE_COUNT {
+                let norm_pos_x = ((x as Real) + rng.gen_range(-1.0, 1.0))/(BUFFER_WIDTH as Real) * 2.0 - 1.0; // [-1:1]
+                let norm_pos_y = ((y as Real) + rng.gen_range(-1.0, 1.0))/(BUFFER_HEIGHT as Real) * 2.0 - 1.0; // [-1:1]
+                let mut pos = Complex {r: 0.0, i: 0.0};
+                pos.r = CENTER_X + norm_pos_x * VIEW_WIDTH / 2.0; // real axis
+                pos.i = CENTER_Y + norm_pos_y * VIEW_HEIGHT / 2.0; // imaginary axis
+                let mut iterations: u32 = 0;
+                let mut temp = Complex {r: 0.0, i: 0.0};
+                while temp.length() <= MAX_LENGTH && iterations < MAX_ITERATIONS {
+                    temp = temp.squared().add(&pos);
+                    iterations += 1;
+                }
+                pixel_color.add(COLOR_PALETTE[(iterations%16) as usize]);
             }
-            color_buffer[y * BUFFER_WIDTH + x] = COLOR_PALETTE[(iterations%16) as usize];
+            pixel_color.divide(SAMPLE_COUNT as Real);
+            color_buffer[y * BUFFER_WIDTH + x] = pixel_color;
         }
     }
 
